4475c732065edf055198e0fa745764a01c261e9d
ejg <egillcoat@gmail.com>
2024-03-18 13:47:15 -0500

    CSS background image in app.vue - works

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div></div>
 </template>
+<style scoped>
+div {
+  width: 200px;
+  height: 186px;
+  background: transparent center / contain url(../images/fl.png) no-repeat;
+}
+</style>
